c327059c3b6ee5ff0095222782e04636fa5e4701
slowisfast <slowisfast2030@163.com>
2023-08-04 10:44:31 +0800

    slow is fast

--- udemy/test.py
+++ udemy/test.py
@@ -41,19 +41,6 @@ class test(Scene):
 		title = Text("3D Graphs").shift(UP*3.5)
 		self.play(FadeIn(title))
 
-		#######
-		#FROM LAST LECTURE
-		#######
-
-		"""
-		#Axes
-		axes = Axes(**self.axes_kwargs)
-		axes.add_coordinate_labels(font_size=20, num_decimal_places=1)
-		func = lambda q: (q-4.5)**2 #Not a VMobject
-		graph = axes.get_graph(func, color=BLUE, step_size=0.001) #VMobject
-		self.play(FadeIn(axes), FadeIn(graph))
-		"""
-
 		#stuff in scene
 		obj1 = Cube().shift(LEFT*4 + IN*4).set_color(GREEN)
 		obj2 = Prism().shift(RIGHT*3 + UP*1 + OUT*1).set_color(RED)
@@ -75,12 +62,6 @@ class test(Scene):
 				frame.increment_theta(0.01)
 				self.wait(0.001)
 				#pass
-		#######
-		#FROM LAST LECTURE
-		#######
-		
-		#self.play(FadeOut(Group(axes, graph)))
-		#rotateScene()
 
 		axes3d = ThreeDAxes(**self.axes3d_kwargs)
 		self.play(FadeIn(axes3d))
@@ -92,33 +73,3 @@ class test(Scene):
 		self.play(ShowCreation(graph, run_time=3))
 		
 		rotateScene()
-
-
-
-
-
-
-
-
-
-
-
-        
-		# func3 = lambda t: np.array([
-		# 	5*np.cos(t)*np.cos(4*t)*np.cos(s)**9,
-		# 	5*np.sin(t)*np.cos(4*t)*np.cos(0.5)**9,
-		# 	5*np.sin(0.5)*np.cos(4*t)*np.cos(0.5)**8])
-		# graph = axes3d.get_parametric_curve(func3, color=BLUE, step_size=0.001, t_range=[-10, 10]) #VMobject
-		# self.play(FadeIn(graph))
-		# rotateScene()
-
-
-
-
-
-
-
-
-
-
-
